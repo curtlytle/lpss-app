bc3cf7fdefbb50448111e1a3ff186d88c78dd42f
Curt Lytle <curt.lytle@idahosif.org>
2021-04-21 16:30:32 -0600

    first commit

---
+++ src/main/java/com/curt/liveprojectspringsecurity/LPSSApplication.java
@@ -0,0 +1,13 @@
+package com.curt.liveprojectspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LPSSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LPSSApplication.class, args);
+    }
+
+}
